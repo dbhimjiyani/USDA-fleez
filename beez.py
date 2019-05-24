51e06fdf456780e8dbc5d35a580cc66a67381ff4
dbhimjiyani <dbhimjiyani@users.noreply.github.com>
2019-05-24 13:05:30 -1000

    SO! Howsit u fakas. Today we are ables to split our data into to train and test sets 70/30 rat. The prediction is absolute garbage but you kno what I worked real hard today to even get to point where this dumb machine was predicting thangz. Man, flattening all these varying ass images was a pain to figure out I'll sure tell ya dat. But it was fun, because I like besting the machine in a game of wits (insert dramatic laugh here). Anyway, I'll optimize the prec=diction model soon but not now because its a beautiful day and I live in Hawai'i. Therefore, I am yeeting myself out this room and into the nearest water body. -Devanshi

--- beez.py
+++ beez.py
@@ -32,19 +32,22 @@ from sklearn.metrics import roc_curve, auc, accuracy_score
 # Check the directory. Just so we know exactly where we is u kno
 print(os.listdir())
 
-# A List of images here, we're making it dynamically by grabbing it from a folder (here it is photo)
+# A List of images here, we're making it dynamically by grabbing it from a folder called photo
 image_paths = [Path(item) for i in [glob.glob('photo\\*.%s' % ext) for ext in ["jpg", "jpeg", "tiff", "tif"]] for item in i]
 
-# load the csv file containing labels using pandas
-labels = pd.read_csv('saved/fleezData.csv', index_col=1)
+# Load the csv file containing labels using pandas
+labels = pd.read_csv('photo/fleezData.csv', header = 0)
 
-# show the first five rows of the dataframe using head
-display(labels.head())
-
+# Get image, resize then return in an array form
+'''Get the image from a given path, 
+resize to a 2:1 ratio for even num of px in all of them,
+return it in a numpy array'''
 def get_image(path):
     img = Image.open(path)
-    return np.array(img)
+    image = img.resize((200, 100), Image.LANCZOS)
+    return np.array(image)
 
+# create features from a numpy array of an image
 def create_features(img):
     # flatten three channel color image
     color_features = img.flatten()
@@ -56,32 +59,30 @@ def create_features(img):
     flat_features = np.hstack((color_features, hog_features))
     return flat_features
 
-
+# Create a matrix of the features
+'''Feed a path into get_image, get the numpy array,
+ then feed that into create_features to process,
+ then append onto on one giant array that is then made into a matrix.
+ NOTE: For this to work properly into the next step, you must have the same number of features in EVERY
+ SINGLE IMAGE in your folder. Hence the resize in the get_image method'''
 def create_feature_matrix():
     features_list = []
 
     for im in image_paths:
         # load image
-        print("getting img")
         img = get_image(im)
-        print(str(im))
         # get features for image
-        print("gettin da features, brah")
         image_features = create_features(img)
-        print(image_features.shape)
         features_list.append(image_features)
-        print("appEND")
 
     # convert list of arrays into a matrix
-    print("'features list length '")
-    print(len(features_list))
     feature_matrix = np.array(features_list)
 
     return feature_matrix
 
-# # run create_feature_matrix on our dataframe of images
+# Run create_feature_matrix on our dataframe of images
 try:
-    if os.path.getsize('saved/fleezData.csv') > 0:
+    if os.path.getsize('photo/fleezData.csv') > 0:
         print("Entering ze luup")
         feature_matrix = create_feature_matrix()
 
@@ -105,7 +106,7 @@ try:
             writer.writerow([text, fleezId, species, view])
 
 
-        with open('saved/fleezData.csv', mode='w') as fleez:
+        with open('photo/fleezData.csv', mode='w') as fleez:
             fleez_writer = csv.writer(fleez, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             fleez_writer.writerow(['FileName', 'FleezId', 'Species', 'View'])
             # for loop over image paths
@@ -120,11 +121,56 @@ except OSError as e:
 ss = StandardScaler()
 # run this on our feature matrix
 fleez_stand = ss.fit_transform(feature_matrix)
-
-pca = PCA(n_components=500)
+pca = PCA(n_components=315)
 # use fit_transform to run PCA on our standardized matrix
 fleez_pca = pca.fit_transform(fleez_stand)
 print('PCA matrix shape is: ', fleez_pca.shape)
+
+# Split data into train and test sets here. Test size is 30% of the total data
+X_train, X_test, y_train, y_test = train_test_split(fleez_pca,
+                                                    labels['Species'].values,
+                                                    test_size=.3,
+                                                    random_state=1234123)
+
+# look at the distrubution of labels in the train set
+print(pd.Series(y_train).value_counts())
+
+# define support vector classifier
+svm = SVC(kernel='linear', probability=True, random_state=42)
+
+# fit model
+svm.fit(X_train, y_train)
+
+# generate predictions
+y_pred = svm.predict(X_test)
+
+# calculate accuracy
+accuracy = accuracy_score(y_pred, y_test)
+print('Model accuracy is: ', accuracy)
+
+# predict probabilities for X_test using predict_proba
+probabilities = svm.predict_proba(X_test)
+
+# select the probabilities for label 1.0
+y_proba = probabilities[:, 1]
+
+# calculate false positive rate and true positive rate at different thresholds
+false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_proba, pos_label=1)
+
+# calculate AUC
+roc_auc = auc(false_positive_rate, true_positive_rate)
+
+plt.title('Receiver Operating Characteristic')
+# plot the false positive rate on the x axis and the true positive rate on the y axis
+roc_plot = plt.plot(false_positive_rate,
+                    true_positive_rate,
+                    label='AUC = {:0.2f}'.format(roc_auc))
+
+plt.legend(loc=0)
+plt.plot([0,1], [0,1], ls='--')
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate');
+plt.show()
 
 # displaying the HOG of a single, final, greyscaled image
 '''# run HOG using our greyscale bombus image
