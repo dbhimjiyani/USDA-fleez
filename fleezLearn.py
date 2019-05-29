312e15fccc2a4916a2c96e8d8569ded0ffe30f68
dbhimjiyani <dbhimjiyani@users.noreply.github.com>
2019-05-28 15:34:29 -1000

    Updated the new deep learn model file so that we can run a mock training model. Now gonna pretrain a model fully and save it but nOT NOW bc it is a beautiful day in Hawai`i and I must take booty to gym with boyf so we can get STRONK. Also made some changes to makeDaCSV because I have to everything around this gotdamn hoise. Until then - Devanshi

--- fleezLearn.py
+++ fleezLearn.py
@@ -12,9 +12,10 @@ import pickle
 from pathlib import Path
 from skimage import io
 import glob
-
+from skimage.transform import resize
 import pandas as pd
 import numpy as np
+from PIL import Image
 
 import matplotlib.pyplot as plt
 
@@ -28,24 +29,31 @@ from keras.models import Sequential
 # import Dense, Dropout, Flatten, Conv2D, MaxPooling2D from the keras layers module
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 
-# Load the csv file containing labels using pandas
+# Load the csv file
 labels = pd.read_csv('photo/fleezData.csv', header = 0)  # <--------------- look here should you change the csv from the default
 
-# print value counts for genus
-print(labels.Species.value_counts()) # <--------------- look here should you change the csv from the default
+# print value counts for species number
+print(labels.FileName.value_counts()) # <--------------- look here should you change the csv from the default
 
 # A List of images here, we're making it dynamically by grabbing it from a folder called photo
 image_paths = [Path(item) for i in [glob.glob('photo\\*.%s' % ext) for ext in ["jpg", "jpeg", "tiff", "tif"]] for item in i]
 
 # assign the species label values to y
-y = labels.Species.values # <--------------- look here should you change the csv from the default
+y = labels.Fleez.values # <--------------- look here should you change the csv from the default
 # initialize standard scaler
 ss = StandardScaler()
+
+def get_image(path):
+    img = Image.open(path)
+    image = img.resize((50, 50), Image.LANCZOS)  # <--------------- look here should you change the csv from the default
+    return np.array(image)
 
 image_list = []
 for i in labels.index:
     # load image
-    img = io.imread(image_paths[i]).astype(np.float64)
+    ''' This needs to call separate function isntead of doing an io.imread here for some reason as we aren't able to get
+    the correct matrix shape for if don't keep the two processes separate. Really bizarre but I mean hey, it works '''
+    img = get_image(image_paths[i]).astype(np.float64)
 
     # for each channel, apply standard scaler's fit_transform method
     for channel in range(img.shape[2]):
@@ -58,4 +66,75 @@ for i in labels.index:
 X = np.array(image_list)
 
 # print shape of X
+''' to make sure we are on the right track. This should read as four column values with the last value being
+ 3 (that is the value for the number of color channels that exist in our image list). IMPORTANT: if only one value
+ shows up, that means that there isn't a standard pixel size of the image list, and the matrix has taken everything in
+ as one giant column of array entry data. Not good as the model will crash if that is the case. '''
 print(X.shape)
+
+# split out evaluation sets (x_eval and y_eval)
+x_interim, x_eval, y_interim, y_eval = train_test_split(X,
+                                           y,
+                                           test_size=0.2, # percent of our data that we want to use for testing the prediction model
+                                           random_state=52)
+
+# split remaining data into train and test sets
+x_train, x_test, y_train, y_test = train_test_split(x_interim,
+                                           y_interim,
+                                           test_size=0.4,
+                                           random_state=52)
+
+# examine number of samples in train, test, and validation sets
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_eval.shape[0], 'eval samples')
+
+# set model constants
+num_classes = 1
+
+# define model as Sequential
+model = Sequential()
+
+# first convolutional layer with 32 filters
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(50, 50, 3)))
+# add a second 2D convolutional layer with 64 filters
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+# reduce dimensionality through max pooling
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# third convolutional layer with 64 filters
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+# add dropout to prevent over fitting
+model.add(Dropout(0.25))
+# necessary flatten step preceeding dense layer
+model.add(Flatten())
+# fully connected layer
+model.add(Dense(128, activation='relu'))
+
+# add additional dropout to prevent overfitting
+model.add(Dropout(0.5))
+
+# prediction layers
+model.add(Dense(num_classes, activation="sigmoid", name='preds'))
+
+# show model summary
+model.summary()
+
+
+# Now we compile and train model
+model.compile(
+    # set the loss as binary_crossentropy
+    loss=keras.losses.binary_crossentropy,
+    # set the optimizer as stochastic gradient descent
+    optimizer=keras.optimizers.SGD(lr=0.001),
+    # set the metric as accuracy
+    metrics=['accuracy']
+)
+
+# mock-train the model using the first ten observations of the train and test sets
+model.fit(
+    x_train[:10, :, :, :],
+    y_train[:10],
+    epochs=5,
+    verbose=1,
+    validation_data=(x_test[:10, :, :, :], y_test[:10]) #hnnGH validate me zaddy
+)
--- makeDaCSV.py
+++ makeDaCSV.py
@@ -13,24 +13,29 @@ def createCSV(path, writer):
     text = re.sub(r'photo\\', '', str(path))
     # Regex for extracting details from name of the file, suhc as the species type, code and the angle of shot
     matchObj = re.match(
-        r'([a-zA-Z0-9]+|(B\_[a-zA-Z0-9\-]+\_[a-zA-Z0-9\-]+)|([a-zA-Z0-9\-]+\_[a-zA-Z0-9\-]+)(\_CostaNotExp)*)\_((ms|FF)[0-9\-A-Za-z]+(_ZN)*)\_([a-z])',
+        r'([a-zA-Z0-9]+|(B\_[a-zA-Z0-9\-]+\_[a-zA-Z0-9\-]+)|([a-zA-Z0-9\-]+\_[a-zA-Z0-9\-]+)(\_CostaNotExp)*)\_((ms|FF)([0-9]+)([\-A-Za-z0-9]*)((_|-)ZN)*)\_([a-z])',
         text)
     try:
+        flee = matchObj.group(7)
         species = matchObj.group(1)
         fleezId = matchObj.group(5)
-        view = matchObj.group(8)
+        view = matchObj.group(11)
     except AttributeError:
-        species = ""
-        fleezId = ""
-        view = ""
-    writer.writerow([text, fleezId, species, view])
+        flee = "0"
+        species = "Error_Species"
+        fleezId = "Error"
+        view = "Error"
+    writer.writerow([flee, fleezId, species, view, text])
+    print(flee + "     " + text)
 
 
 with open('photo/fleezData.csv', mode='w') as fleez:
     fleez_writer = csv.writer(fleez, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    fleez_writer.writerow(['FileName', 'FleezId', 'Species', 'View'])
+    fleez_writer.writerow(['Fleez', 'FullFleezId', 'Species', 'View', 'FileName'])
     # for loop over image paths
     for img_path in image_paths:
-        file = createCSV(Path(img_path), fleez_writer)
+        createCSV(Path(img_path), fleez_writer)
+
 print("youe fool. creating the csv my damn self because nobody helps me in this household")
-print()
+
+
